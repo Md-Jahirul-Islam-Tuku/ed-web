0bba99233aa5e83a86a922e15d8dbd9f6aad3e92
Tuku Webian <tukuwebian@gmail.com>
2022-10-26 11:36:51 +0600

    Router and Navbar create

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SideBar = () => {
+    return (
+        <div className="border-r-4 border-slate-50 h-screen">
+            <h1>This is Side Bar</h1>
+        </div>
+    )
+}
+
+export default SideBar;
---
+++ src/layout/Main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import SideBar from "../components/SideBar";
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div className="grid grid-cols-4">
+                <div><SideBar></SideBar></div>
+                <div className="col-span-3"><Outlet></Outlet></div>
+            </div>
+        </div>
+    )
+}
+
+export default Main;
